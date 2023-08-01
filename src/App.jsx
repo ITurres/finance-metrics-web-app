48408464beee1d1765c29eb2215ee01a2ae4aebf
iturres <emanueliturres.co@gmail.com>
2023-08-01 14:01:19 -0300

    mod: on 'App' Imported 'Home' and 'CompanyPage' -components- / Removed useSelector

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { React, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
 
 import fetchCompanies from './services/FMPAPI/FMPAPI';
 import Header from './components/Header';
+import Home from './pages/Home';
+import CompanyPage from './pages/CompanyPage';
 
 const App = () => {
   const dispatch = useDispatch();
-  const companies = useSelector((state) => state.companies);
-
-  console.log(companies);
 
   useEffect(() => {
     dispatch(fetchCompanies());
@@ -19,7 +18,10 @@ const App = () => {
     <header>
       <Header />
 
-      <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/company" element={<CompanyPage />} />
+      </Routes>
     </header>
   );
 };

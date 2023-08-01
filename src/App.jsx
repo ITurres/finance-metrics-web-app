338e5c3382a46b1ac9fdc97bb27c3a1204158c84
iturres <emanueliturres.co@gmail.com>
2023-07-31 22:14:36 -0300

    feat: modify 'App' to dispatch and select data when fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import { React, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Routes } from 'react-router-dom';
 
-function App() {
-  return <></>;
-}
+import fetchCompanies from './services/FMPAPI/FMPAPI';
+import Header from './components/Header';
+
+const App = () => {
+  const dispatch = useDispatch();
+  const companies = useSelector((state) => state.companies);
+
+  console.log(companies);
+
+  useEffect(() => {
+    dispatch(fetchCompanies());
+  }, [dispatch]);
+
+  return (
+    <header>
+      <Header />
+
+      <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
+    </header>
+  );
+};
 
 export default App;

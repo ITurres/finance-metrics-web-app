13f0a4cbceb6114fcb34d38fd4f353f92c6abf66
iturres <emanueliturres.co@gmail.com>
2023-07-31 22:12:05 -0300

    feat: Import and initialize 'companiesReducer' on 'store' reducers

--- src/redux/config-store.js
+++ src/redux/config-store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import companiesReducer from './companies/companiesSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    companiesReducer,
+  },
 });
 
 export default store;

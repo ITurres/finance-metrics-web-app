ca0357b33b043241005a2a85de2a8defb440478e
iturres <emanueliturres.co@gmail.com>
2023-08-02 14:22:28 -0300

    feat: on 'companiesSlice.js' Modify error state when fetch rejected

--- src/redux/companies/companiesSlice.js
+++ src/redux/companies/companiesSlice.js
@@ -22,7 +22,7 @@ const companiesSlice = createSlice({
       })
       .addCase(fetchCompanies.rejected, (state, action) => {
         state.loading = false;
-        state.error = action.payload;
+        state.error = action.payload?.error || 'Unknown error occurred';
       });
   },
 });
